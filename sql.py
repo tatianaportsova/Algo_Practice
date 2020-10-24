c9faf0feabcf1331e13efdd004eec070df092aa2
tatianaportsova <tt.sova@gmail.com>
2020-10-23 22:45:48 -0400

    sql string

--- sql.py
+++ sql.py
@@ -5,3 +5,13 @@ select distinct city from station where city REGEXP "^[aeiou].*";
 '''
 select distinct city from station where SUBSTR(city,-1,1) in ('a','e','i','o','u');
 '''
+
+'''
+select distinct city
+    from station where SUBSTR(city,1,1) in ('A','E','I','O','U') and 
+    SUBSTR(city,-1,1) in ('a','e','i','o','u');
+'''
+
+'''
+select distinct city from station where substr(city,-1,1) not in ('a','e','i','o','u');
+'''
