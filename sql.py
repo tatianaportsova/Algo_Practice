285ddad3f8b6ebbe357ce53a0178165979121521
tatianaportsova <tt.sova@gmail.com>
2020-10-25 10:00:38 -0400

    sql

--- sql.py
+++ sql.py
@@ -15,3 +15,8 @@ select distinct city
 '''
 select distinct city from station where substr(city,-1,1) not in ('a','e','i','o','u');
 '''
+
+'''
+select distinct city 
+    from station where substr(city, 1,1) not in ('A', 'E', 'I', 'O', 'U') or substr(city, -1,1)             not in ('a', 'e', 'o', 'u', 'i');
+'''
