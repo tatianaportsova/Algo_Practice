5f4df87986328ab0cf8702464cb6b591ca7f8477
tatianaportsova <tt.sova@gmail.com>
2020-10-28 20:49:40 -0400

    SQL

--- sql.py
+++ sql.py
@@ -87,3 +87,41 @@ Query the Euclidean Distance between points P1 and P2 and format your answer to 
 
 select round(sqrt(power(max(lat_n)-min(lat_n), 2)+power(max(long_w)-min(long_w), 2)), 4)from station;
 '''
+
+'''
+Write a query identifying the type of each record in the TRIANGLES table using its three side lengths. Output one of the following statements for each record in the table:
+
+Equilateral: It's a triangle with 3 sides of equal length.
+Isosceles: It's a triangle with 2 sides of equal length.
+Scalene: It's a triangle with 3 sides of differing lengths.
+Not A Triangle: The given values of A, B, and C don't form a triangle.
+
+SELECT CASE
+WHEN A + B > C THEN CASE WHEN A = B AND B = C THEN 'Equilateral' WHEN A = B OR B = C OR A = C THEN 'Isosceles' WHEN A != B OR B != C OR A != C THEN 'Scalene' END
+ELSE 'Not A Triangle' END FROM TRIANGLES;
+'''
+
+'''
+Query an alphabetically ordered list of all names in OCCUPATIONS, 
+immediately followed by the first letter of each profession as a parenthetical 
+(i.e.: enclosed in parentheses). 
+For example: AnActorName(A), ADoctorName(D), AProfessorName(P), and ASingerName(S).
+Query the number of ocurrences of each occupation in OCCUPATIONS. 
+Sort the occurrences in ascending order, and output them in the following format:
+
+There are a total of [occupation_count] [occupation]s.
+
+where [occupation_count] is the number of occurrences of an occupation in OCCUPATIONS 
+and [occupation] is the lowercase occupation name. 
+If more than one Occupation has the same [occupation_count], they should be ordered alphabetically.
+
+
+SELECT Name || '(' || SUBSTR(Occupation, 1, 1) || ')'
+FROM OCCUPATIONS
+ORDER BY Name;
+
+SELECT 'There are a total of ' || Count(Name) || ' ' || LOWER(Occupation) || 's.'
+FROM Occupations
+GROUP BY Occupation
+ORDER BY Count(Name), Occupation;
+'''
