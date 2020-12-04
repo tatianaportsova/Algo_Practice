3a0ee3121ece544511c512b9d1e4c0c80b6e2f6e
tatianaportsova <tt.sova@gmail.com>
2020-12-03 22:34:16 -0500

    New sql

--- sql2.py
+++ sql2.py
@@ -7,3 +7,9 @@ The sum of all values in LONG_W rounded to a scale of 2 decimal places.
 select round(sum(lat_n), 2), round(sum(long_w), 2) from station;
 '''
 
+'''
+Query the sum of Northern Latitudes (LAT_N) from STATION having values 
+greater than 38.7880 and less than 137.2345. Truncate your answer to 4 decimal places.
+
+select round(sum(lat_n), 4) from station where lat_n>38.7880 and lat_n<137.2345;
+'''
