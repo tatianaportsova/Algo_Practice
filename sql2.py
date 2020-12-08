fe6bb4503734c9bfcee1b48d7aa898b15cb0bd44
tatianaportsova <tt.sova@gmail.com>
2020-12-07 22:07:39 -0500

    sql

--- sql2.py
+++ sql2.py
@@ -28,3 +28,31 @@ Query the difference between the maximum and minimum populations in CITY.
 
 select max(population)-min(population) as difference from city;
 '''
+
+'''
+Samantha was tasked with calculating the average monthly salaries for all employees 
+in the EMPLOYEES table, but did not realize her keyboard's 0 key was broken until after 
+completing the calculation. 
+She wants your help finding the difference between her miscalculation 
+(using salaries with any zeroes removed), and the actual average salary.
+
+Write a query calculating the amount of error (i.e.: actual-miscalculated average monthly salaries), 
+and round it up to the next integer.
+
+select ceil(avg(salary)-avg(replace(salary, '0',''))) from employees;
+
+The CEIL() function returns the smallest integer value that is bigger than or equal to a number.
+
+Note: This function is equal to the CEILING() function.
+'''
+
+'''
+We define an employee's total earnings to be their monthly salary*months worked, 
+and the maximum total earnings to be the maximum total earnings for any employee 
+in the Employee table. 
+Write a query to find the maximum total earnings for all employees as well as 
+the total number of employees who have maximum total earnings. 
+Then print these values as 2 space-separated integers.
+
+select (salary * months)as earnings, count(*) from employee group by 1 order by earnings desc limit 1;
+'''
