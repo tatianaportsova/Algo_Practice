8115e41dd7c847ef20bf6a63abf0b7020b7782b7
tatianaportsova <tt.sova@gmail.com>
2020-12-08 23:12:09 -0500

    one more sql

--- sql2.py
+++ sql2.py
@@ -64,3 +64,13 @@ select sum(city.population) from city
 join country on city.countrycode=country.code
 where country.continent='Asia';
 '''
+
+'''
+Given the CITY and COUNTRY tables, query the names of all cities where the CONTINENT is 'Africa'.
+Note: CITY.CountryCode and COUNTRY.Code are matching key columns.
+
+select city.name from city
+join country on city.countrycode=country.code
+where country.continent='Africa';
+'''
+
