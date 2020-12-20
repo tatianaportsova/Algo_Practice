cf2abd5e2e5d26443a65616ec6f5f2248ae764d9
tatianaportsova <tt.sova@gmail.com>
2020-12-19 22:23:35 -0500

    python practice

--- python_problem.py
+++ python_problem.py
@@ -147,3 +147,24 @@ def plusMinus(arr):
     print(neg/len(arr))
     print(net/len(arr))
 
+
+'''
+The provided code stub will read in a dictionary containing key/value pairs 
+of name:[marks] for a list of students. 
+Print the average of the marks array for the student name provided, 
+showing 2 places after the decimal.
+'''
+
+if __name__ == '__main__':
+    n = int(raw_input())
+    student_marks = {}
+    for _ in range(n):
+        line = raw_input().split()
+        name, scores = line[0], line[1:]
+        scores = map(float, scores)
+        student_marks[name] = scores
+    query_name = raw_input()
+    p =  sum(student_marks[query_name])/3
+    print(format(p, '.2f'))
+
+
