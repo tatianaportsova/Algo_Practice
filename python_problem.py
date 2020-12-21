4ef6a96c5497fdd46b35a2e4ecc848d918428be7
tatianaportsova <tt.sova@gmail.com>
2020-12-20 23:23:24 -0500

    printing strings python

--- python_problem.py
+++ python_problem.py
@@ -168,3 +168,11 @@ if __name__ == '__main__':
     print(format(p, '.2f'))
 
 
+"""
+You are given the firstname and lastname of a person on two different lines. 
+Your task is to read them and print the following:
+  Hello firstname lastname! You just delved into python.
+"""
+
+def print_full_name(a, b):
+    print "Hello {} {}! You just delved into python.".format(a,b)
