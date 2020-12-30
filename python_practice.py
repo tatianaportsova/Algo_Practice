27b493b4be7ab20366e0d0a6761db9324a1ad65f
tatianaportsova <tt.sova@gmail.com>
2020-12-29 22:29:58 -0500

    python problem solving

--- python_practice.py
+++ python_practice.py
@@ -67,3 +67,23 @@ regex_pattern = r",|\."
 
 import re
 print("\n".join(re.split(regex_pattern, input())))
+
+"""
+HackerLand University has the following grading policy:
+
+Every student receives a grade in the inclusive range from 0 to 100.
+Any grade less than 40 is a failing grade.
+Sam is a professor at the university and likes to round each student's grade according to these rules:
+
+If the difference between the grade and the next multiple of 5 is less than 3, round grade up to the next multiple of 5.
+If the value of grade is less than 38, no rounding occurs as the result will still be a failing grade.
+"""
+
+def gradingStudents(grades):
+    for g in range(0, len(grades)):
+        if grades[g] >= 38:
+            if (grades[g]+1)%5 == 0:
+                grades[g] = grades[g]+1
+            elif (grades[g]+2)%5 == 0:
+                grades[g] = grades[g]+2
+    return grades
