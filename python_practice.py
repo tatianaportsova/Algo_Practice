d88ff570d5090d49bf8ba367d93cacb5092f314a
tatianaportsova <tt.sova@gmail.com>
2020-12-31 00:11:44 -0500

    count distance

--- python_practice.py
+++ python_practice.py
@@ -88,3 +88,29 @@ def gradingStudents(grades):
                 grades[g] = grades[g]+2
     return grades
 
+"""
+Sam's house has an apple tree and an orange tree that yield an abundance of fruit. 
+Using the information given below, determine the number of apples and oranges that land on Sam's house.
+
+In the diagram below:
+
+The red region denotes the house, where s is the start point, and t is the endpoint. 
+The apple tree is to the left of the house, and the orange tree is to its right.
+Assume the trees are located on a single point, where the apple tree is at point a, 
+and the orange tree is at point b.
+When a fruit falls from its tree, it lands d units of distance from its tree of origin 
+along the -axis. *A negative value of d means the fruit fell d units to the tree's left,
+ and a positive value of d means it falls d units to the tree's right. *
+ """
+
+ def countApplesAndOranges(s, t, a, b, apples, oranges):
+    apples_ = 0
+    for i in apples:
+        if (i+a)>=s and (i+a)<=t:
+            apples_ += 1
+    oranges_ = 0
+    for i in oranges:
+        if (i+b)>=s and (i+b)<=t:
+            oranges_ += 1
+    print(apples_)
+    print(oranges_)
