1a532fa4f1d0757ed282b71fed182b2ac14a6d46
tatianaportsova <tt.sova@gmail.com>
2021-01-02 11:48:26 -0500

    str alignment

--- python_practice.py
+++ python_practice.py
@@ -124,3 +124,45 @@ convert all lowercase letters to uppercase letters and vice versa.
 def swap_case(s):
     return s.swapcase()
 
+"""
+You are given a string. 
+Split the string on a " " (space) delimiter and join using a - hyphen.
+"""
+
+def split_and_join(line):
+    line = line.split(" ")
+    return "-".join(line)
+
+
+"""
+In Python, a string of text can be aligned left, right and center.
+
+You are given a partial code that is used for generating the HackerRank Logo of variable thickness.
+Your task is to replace the blank (______) with rjust, ljust or center.
+"""
+
+#Replace all ______ with rjust, ljust or center. 
+
+thickness = int(input()) #This must be an odd number
+c = 'H'
+
+#Top Cone
+for i in range(thickness):
+    print((c*i).rjust(thickness-1)+c+(c*i).ljust(thickness-1))
+
+#Top Pillars
+for i in range(thickness+1):
+    print((c*thickness).center(thickness*2)+(c*thickness).center(thickness*6))
+
+#Middle Belt
+for i in range((thickness+1)//2):
+    print((c*thickness*5).center(thickness*6))    
+
+#Bottom Pillars
+for i in range(thickness+1):
+    print((c*thickness).center(thickness*2)+(c*thickness).center(thickness*6))    
+
+#Bottom Cone
+for i in range(thickness):
+    print(((c*(thickness-i-1)).rjust(thickness)+c+(c*(thickness-i-1)).ljust(thickness)).rjust(thickness*6))
+
