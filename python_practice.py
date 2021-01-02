f5b051ef89c0918ae5e944cbd36d5fe5aa106fa2
tatianaportsova <tt.sova@gmail.com>
2021-01-02 11:31:33 -0500

    python swap_case

--- python_practice.py
+++ python_practice.py
@@ -114,3 +114,13 @@ along the -axis. *A negative value of d means the fruit fell d units to the tree
             oranges_ += 1
     print(apples_)
     print(oranges_)
+
+
+"""
+You are given a string and your task is to swap cases. In other words, 
+convert all lowercase letters to uppercase letters and vice versa.
+"""
+
+def swap_case(s):
+    return s.swapcase()
+
