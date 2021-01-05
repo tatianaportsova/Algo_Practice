f81eb6eb7fd6e6ec68748f5bdc6047698093d63e
tatianaportsova <tt.sova@gmail.com>
2021-01-04 21:58:33 -0500

    printing Rangoli

--- python_practice.py
+++ python_practice.py
@@ -189,5 +189,27 @@ if __name__ == '__main__':
     fptr.close()
 
 
+"""
+You are given an integer, N. Your task is to print an alphabet rangoli of size N. 
+(Rangoli is a form of Indian folk art based on creation of patterns.)
+"""
+
+import string
+
+def print_rangoli(size):
+    alpha = string.ascii_lowercase
+
+    L = []
+    for i in range(size):
+        s = "-".join(alpha[i:size])
+        L.append((s[::-1]+s[1:]).center(4*size-3, "-"))
+    print('\n'.join(L[:0:-1]+L))
+
+if __name__ == '__main__':
+    n = int(input())
+    print_rangoli(n)
+
+
+
 
 
