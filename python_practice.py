3be66d7740154827bd813d1a3193ddfc14d20882
tatianaportsova <tt.sova@gmail.com>
2021-01-08 21:42:16 -0500

    itertools

--- python_practice.py
+++ python_practice.py
@@ -230,3 +230,18 @@ if __name__ == '__main__':
     print(result)
 
 
+"""
+itertools.product()
+
+This tool computes the cartesian product of input iterables.
+It is equivalent to nested for-loops.
+For example, product(A, B) returns the same as ((x,y) for x in A for y in B).
+"""
+
+from itertools import product
+
+a = list(map(int, input().split()))
+b = list(map(int, input().split()))
+
+print(*product(a, b))
+
