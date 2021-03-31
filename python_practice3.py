5290c3ce5b41239d97f5c4c6a11787773df75e37
tatianaportsova <tt.sova@gmail.com>
2021-03-30 23:18:52 -0400

    added description of the task

--- python_practice3.py
+++ python_practice3.py
@@ -18,6 +18,9 @@ def average(array):
 
 
 '''
+Given 2 sets of integers, M and N, print their symmetric difference in ascending order.
+The term symmetric difference indicates those values that exist in either M or N
+but do not exist in both.
 '''
 
 a,b=(int(input()),input().split())
