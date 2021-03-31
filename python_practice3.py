796877555f313e102b508b13381e44a49b4d755d
tatianaportsova <tt.sova@gmail.com>
2021-03-30 23:17:25 -0400

    added some python code

--- python_practice3.py
+++ python_practice3.py
@@ -8,10 +8,23 @@ Average has the following parameters:
 Returns:
 -float: the resulting float value rounded to 3 places after the decimal
 
-Input Format:
+Input Format:s
 -The first line contains the integer, N, the size of arr.
 The second line contains the N space-separated integers, arr[i].
 '''
 
 def average(array):
     return round(sum(set(array))/len(set(array)), 3)
+
+
+'''
+'''
+
+a,b=(int(input()),input().split())
+c,d=(int(input()),input().split())
+x=set(b)
+y=set(d)
+p=y.difference(x)
+q=x.difference(y)
+r=p.union(q)
+print ('\n'.join(sorted(r, key=int)))
